d9d5d344984b7565136f47a99274ee06cef430cc
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-09-18 23:26:20 +0530

    Day1-CreateTable&Insert

---
+++ student.sql
@@ -0,0 +1,9 @@
+CREATE TABLE STUDENT (
+    RollNumber int,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Stream varchar(255),
+    Marks int
+);
+INSERT INTO STUDENT
+VALUES (0012020, ABC, Science, 98);
