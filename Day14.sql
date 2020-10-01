0ffb24f1b95dce63538c598916773da8015f4102
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-02 00:47:29 +0530

    wireuschallenge day14

--- Day14.sql
+++ Day14.sql
@@ -1,12 +1,12 @@
--- CREATE TABLE Day14(
---     Admno int,
---     fullName VARCHAR(255),
---     class int,
---     fees VARCHAR(255),
--- );
+CREATE TABLE Day14(
+    Admno int,
+    fullName VARCHAR(255),
+    class int,
+    fees VARCHAR(255),
+);
 
--- INSERT INTO Day14
--- VALUES(101, 'Hemant', 4, 'PAID');
+INSERT INTO Day14
+VALUES(101, 'Hemant', 4, 'PAID');
 
 INSERT INTO Day14
 VALUES(432, 'Mahira', 5, 'PAID');
