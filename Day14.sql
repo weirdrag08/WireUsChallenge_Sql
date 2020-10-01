77c15f50ec67f3e06b0e77333fef94c4b713ea17
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-01 22:10:51 +0530

    wireuschallenge day14

---
+++ Day14.sql
@@ -0,0 +1,45 @@
+-- CREATE TABLE Day14(
+--     Admno int,
+--     fullName VARCHAR(255),
+--     class int,
+--     fees VARCHAR(255),
+-- );
+
+-- INSERT INTO Day14
+-- VALUES(101, 'Hemant', 4, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(432, 'Mahira', 5, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(342, 'RASHMITA', 1, 'NOT PAID');
+
+-- INSERT INTO Day14
+-- VALUES(561, 'Sonalika', 1, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(100, 'Aishwarya', 5, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(677, 'Bharat',7, 'NOT PAID');
+
+-- INSERT INTO Day14
+-- VALUES(980, 'Daksh', 7, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(8, 'Priyanka', 5, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(866, 'Anushree', 9, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(343, 'Chandrika', 3, 'PAID');
+
+-- INSERT INTO Day14
+-- VALUES(775, 'Debnika', 2, 'NOT PAID');
+
+DELETE FROM Day14
+WHERE Admno<100;
+
+SELECT *
+FROM Day14;
