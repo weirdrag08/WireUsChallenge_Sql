42d74fab60866a8f0dd0ec9142a8dd93d97ae4ea
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-02 00:47:01 +0530

    wireuschallenge day14

--- Day14.sql
+++ Day14.sql
@@ -8,35 +8,35 @@
 -- INSERT INTO Day14
 -- VALUES(101, 'Hemant', 4, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(432, 'Mahira', 5, 'PAID');
+INSERT INTO Day14
+VALUES(432, 'Mahira', 5, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(342, 'RASHMITA', 1, 'NOT PAID');
+INSERT INTO Day14
+VALUES(342, 'RASHMITA', 1, 'NOT PAID');
 
--- INSERT INTO Day14
--- VALUES(561, 'Sonalika', 1, 'PAID');
+INSERT INTO Day14
+VALUES(561, 'Sonalika', 1, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(100, 'Aishwarya', 5, 'PAID');
+INSERT INTO Day14
+VALUES(100, 'Aishwarya', 5, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(677, 'Bharat',7, 'NOT PAID');
+INSERT INTO Day14
+VALUES(677, 'Bharat',7, 'NOT PAID');
 
--- INSERT INTO Day14
--- VALUES(980, 'Daksh', 7, 'PAID');
+INSERT INTO Day14
+VALUES(980, 'Daksh', 7, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(8, 'Priyanka', 5, 'PAID');
+INSERT INTO Day14
+VALUES(8, 'Priyanka', 5, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(866, 'Anushree', 9, 'PAID');
+INSERT INTO Day14
+VALUES(866, 'Anushree', 9, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(343, 'Chandrika', 3, 'PAID');
+INSERT INTO Day14
+VALUES(343, 'Chandrika', 3, 'PAID');
 
--- INSERT INTO Day14
--- VALUES(775, 'Debnika', 2, 'NOT PAID');
+INSERT INTO Day14
+VALUES(775, 'Debnika', 2, 'NOT PAID');
 
 DELETE FROM Day14
 WHERE Admno<100;
