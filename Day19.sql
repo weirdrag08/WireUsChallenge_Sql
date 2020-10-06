e696cd2b0684b62c93a26935088d5dc45a9ccb3f
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-06 21:57:18 +0530

    wireus challenge Day19

---
+++ Day19.sql
@@ -0,0 +1,33 @@
+-- CREATE TABLE Day19(
+--     ID int,
+--     f_name VARCHAR(255),
+--     l_name VARCHAR(255),
+--     salary int,
+--     joining_date DATETIME,
+--     department VARCHAR(255),
+-- );
+
+-- INSERT INTO Day19
+-- VALUES(001, 'Monika', 'Arora', 100000, '2014-02-20 09:00:00', ' HR');
+-- INSERT INTO Day19
+-- VALUES(002, 'Niharika', 'Verma', 800000, '2014-06-11 09:00:00', 'Admin');
+-- INSERT INTO Day19
+-- VALUES(003, 'Vishal', 'Singhal', 300000, '2014-02-20 09:00:00', ' HR');
+-- INSERT INTO Day19
+-- VALUES(004, 'Amitabh', 'Singh', 500000, '2014-02-20 09:00:00',' Admin');
+-- INSERT INTO Day19
+-- VALUES(005, 'Vivek', 'Bhati', 500000, '2014-06-11 09:00:00', ' Admin');
+-- INSERT INTO Day19
+-- VALUES(006, 'Vipul', 'Diwan', 200000, '2014-06-11 09:00:00', ' Account ');
+-- INSERT INTO Day19
+-- VALUES(007, 'Satish', 'Kumar', 75000, '2014-01-20 09:00:00', 'Account');
+-- INSERT INTO Day19
+-- VALUES(008, 'Geetika', 'Chauhan', 90000, '2014-04-11 09:00:00', 'Admin ');
+
+
+SELECT LTRIM(department)
+FROM Day19;
+
+SELECT RTRIM(department)
+FROM Day19;
+
