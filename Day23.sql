b6a2af2b2c74d9569756dd4306bb1e8a32327d1e
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-11 20:13:19 +0530

    wireus challenge Day23

---
+++ Day23.sql
@@ -0,0 +1,8 @@
+Ans 1:
+DCL is not supported by sql
+
+Ans 2:
+FOREIGN KEY constraints identify and enforce the relationships between tables
+
+Ans 3:
+Primary keys also enforce uniqueness, but primary keys do not allow for NULL as one of the unique values.
