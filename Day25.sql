cb3c6e4119427dd665fa04fc19fd61c5f603e213
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-13 20:41:07 +0530

    wireus challenge Day25

--- Day25.sql
+++ Day25.sql
@@ -1,8 +1,8 @@
--- Ques 1: how many types of constraints are present in sql server
--- Ans 1: Two 
+Ques 1: how many types of constraints are present in sql server
+Ans 1: Two 
 
--- Ques 2: which of the following foreign key constraint specifies that the deletion fails with an error
--- Ans 2: NO ACTION
+Ques 2: which of the following foreign key constraint specifies that the deletion fails with an error
+Ans 2: NO ACTION
 
--- Ques 3: purpose of foreign key constraint in sql 
--- Ans 3: FOREIGN KEY constraints identify and enforce the relationships between tables.
+Ques 3: purpose of foreign key constraint in sql 
+Ans 3: FOREIGN KEY constraints identify and enforce the relationships between tables.
