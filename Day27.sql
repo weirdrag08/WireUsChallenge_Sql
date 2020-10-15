623bd9ca365ce99751dc8604dbf4fcd4ad21cde1
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-15 22:16:11 +0530

    wireus challenge Day27

--- Day27.sql
+++ Day27.sql
@@ -1,20 +1,20 @@
--- CREATE TABLE Day27(
---     A int,
---     B int,
---     C int
--- );
+CREATE TABLE Day27(
+    A int,
+    B int,
+    C int
+);
 
--- INSERT INTO Day27
--- VALUES(20,20,23);
+INSERT INTO Day27
+VALUES(20,20,23);
 
--- INSERT INTO Day27
--- VALUES(20,20,20);
+INSERT INTO Day27
+VALUES(20,20,20);
 
--- INSERT INTO Day27
--- VALUES(20,21,22);
+INSERT INTO Day27
+VALUES(20,21,22);
 
--- INSERT INTO Day27
--- VALUES(13,14,30);
+INSERT INTO Day27
+VALUES(13,14,30);
 
 SELECT CASE 
 WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle' 
