d2b3df5ec6dc11bb3fa221edff7dd3a8b5cb2903
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-15 22:15:32 +0530

    wireus challenge Day27

---
+++ Day27.sql
@@ -0,0 +1,25 @@
+-- CREATE TABLE Day27(
+--     A int,
+--     B int,
+--     C int
+-- );
+
+-- INSERT INTO Day27
+-- VALUES(20,20,23);
+
+-- INSERT INTO Day27
+-- VALUES(20,20,20);
+
+-- INSERT INTO Day27
+-- VALUES(20,21,22);
+
+-- INSERT INTO Day27
+-- VALUES(13,14,30);
+
+SELECT CASE 
+WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle' 
+WHEN A = B AND B = C THEN 'Equilateral' 
+WHEN A = B OR B = C OR A = C THEN 'Isosceles' 
+ELSE 'Scalene' 
+END 
+FROM Day27;
