0b527b7c2928fe545b9a0f1b2e93939f9b793673
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-20 18:13:22 +0530

    wireus challenge Day32

---
+++ Day32.sql
@@ -0,0 +1,20 @@
+-- 1 ans:)
+-- Firstname      EmpSalary      
+-- Maithili        50000
+-- Robert          45000
+-- Henry           40000
+
+
+-- 2 ans:)
+-- 3
+
+
+-- 3 ans:)
+-- Designation     Sum_Salary
+-- Manager           214500
+-- Clerk             135000
+
+
+-- 4 ans:)
+-- COUNT
+-- 32000
