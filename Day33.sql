fe9fe10fbe97fda1b6bdc99d836e23dc7012f596
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-21 10:17:33 +0530

    wireus challenge Day33

---
+++ Day33.sql
@@ -0,0 +1,12 @@
+Ques 1) Which of the following is not a atype of DBMS?
+Ans 1) loop databases
+
+
+
+Ques 2) Which of the following is a type of inner join?
+Ans 2) All of these
+
+
+
+Ques 3)Which of the following is not for TRUNCATE command is sql?
+Ans 3) Truncate is not a DDL command.
