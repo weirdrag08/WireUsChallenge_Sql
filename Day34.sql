6cc8650e80ca34b15c017640bfa73fd06241254e
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-22 20:27:27 +0530

    wireus challenge Day34

--- Day34.sql
+++ Day34.sql
@@ -1,11 +1,11 @@
--- ques 1:) which of the following is true about Acid properties in database?
--- ans 1:)ACID is an acronym for atomicity, consistency, isolation, durability.
+ques 1:) which of the following is true about Acid properties in database?
+ans 1:)ACID is an acronym for atomicity, consistency, isolation, durability.
 
 
--- ques 2:)which of the following is not the usage of sql functions?
--- ans 2:)to modify individual data items.
+ques 2:)which of the following is not the usage of sql functions?
+ans 2:)to modify individual data items.
 
 
--- ques 3:)which are different case manipulations in sql?
--- ans 3:)All of the above
+ques 3:)which are different case manipulations in sql?
+ans 3:)All of the above
 
