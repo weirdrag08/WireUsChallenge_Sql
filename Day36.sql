e45afc1a14be1f5226c818e0c406860bb1308e3b
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-24 12:10:46 +0530

    wireus challenge Day36

---
+++ Day36.sql
@@ -0,0 +1,5 @@
+Ques 1:) What is a composite key?
+Ans 1:) It is a primary key that consists of more then one field that uniquely identifies a record in the database.
+
+Ques 2:) Which of the following is correct?
+Ans 2:) UNION operation is used to combine the result sets from more than one query.
