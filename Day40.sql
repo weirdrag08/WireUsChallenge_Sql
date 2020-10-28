aee13c8ac43d4e2391528a5fb7d1494bf82a25ae
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-28 10:35:55 +0530

    wireus challenge Day40

---
+++ Day40.sql
@@ -0,0 +1,8 @@
+Ques 1:) Which of the following is true about the PL/SQL language?
+Ans :) It has a procedural nature.
+
+Ques 2:)Which of the following is not a valid syntax for various manipulationfunctions available in sql?
+Ans 2:) CASE('string')
+
+Ques 3:) What is/are most basic MYSQL components?
+Ans 3:)All of the above
