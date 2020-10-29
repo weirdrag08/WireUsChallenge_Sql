06fc7d3b7bd4b62d1a04ce0585c6512db9f57288
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-29 11:07:58 +0530

    wireus challenge Day41

---
+++ Day41.sql
@@ -0,0 +1,8 @@
+Ques 1:) How would you check current version of SQL?
+Ans :) SELECT VERSION()
+
+Ques 2:) For what recursive stored procedure is mainly used?
+Ans:) It is a procedure that will make a code calls itself until specific boundary condition is reached 
+
+Ques 3:)Correct statement to retrieve common records from two tables
+Ans:) SELEcT Employye id from employee INTERSECT SELEcT Employye id from workshift
