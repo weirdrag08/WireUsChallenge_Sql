9eccc29f03fe6d25097d0341ffa3bac56a32e9f9
Aditya Anand <60457205+weirdrag08@users.noreply.github.com>
2020-12-20 10:06:13 +0530

    wireuschallenege

---
+++ Day49.sql
@@ -0,0 +1,5 @@
+Ques 1:) FIRST()
+
+Ques 2:) It converts the value of the field to uppercase
+
+Ques 3:) only supports fetching rows from the start to the end of the result set.
---
+++ Day50.sql
@@ -0,0 +1,5 @@
+Ques 1:)the set-based solution should be used as it is much faster and has linear scaling.
+
+Ques 2:)Sybase & Microsoft
+
+Ques 3:)All of the above
